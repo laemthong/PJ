0d35f69adfedce89d49dc78f9bd9e3b23749a1a8
Your Name <you@example.com>
2024-09-30 20:03:06 +0700

    แก้ไขรายงาน

--- printPDF.php
+++ printPDF.php
@@ -44,22 +44,49 @@ if ($reportType == 'location') {
                 <tr>
                     <th>ลำดับ</th>
                     <th>ชื่อสถานที่</th>
-                    <th>เวลาเปิด</th>
                     <th>ละติจูด</th>
                     <th>ลองจิจูด</th>
+                    <th>ประเภทสนามกีฬา</th>
+                    <th>วันทำการ</th>
+                    <th>เวลาเปิด-ปิด</th>
+                    <th>จำนวนครั้งที่ถูกเรียกใช้</th>
                 </tr>';
 
-    $query = "SELECT location_name, location_time, latitude, longitude FROM location";
+    $query = "
+        SELECT 
+            location.location_name, 
+            location.latitude, 
+            location.longitude, 
+            sport_type.type_name, 
+            location.location_day, 
+            location.location_time, 
+            COUNT(activity.activity_id) AS usage_count
+        FROM location
+        LEFT JOIN sport_type_in_location ON location.location_id = sport_type_in_location.location_id
+        LEFT JOIN sport_type ON sport_type_in_location.type_id = sport_type.type_id
+        LEFT JOIN activity ON location.location_id = activity.location_id
+        GROUP BY location.location_id
+    ";
     $result = mysqli_query($conn, $query);
     $counter = 1;
     
     while ($row = mysqli_fetch_assoc($result)) {
+        // แปลงวันทำการเป็นชื่อวัน
+        $dayNumbers = explode(',', $row['location_day']);
+        $dayNames = array('อาทิตย์', 'จันทร์', 'อังคาร', 'พุธ', 'พฤหัสบดี', 'ศุกร์', 'เสาร์');
+        $dayList = implode(', ', array_map(function($dayNumber) use ($dayNames) {
+            return $dayNames[trim($dayNumber)];
+        }, $dayNumbers));
+
         $html .= '<tr>
                     <td>' . $counter . '</td>
                     <td>' . htmlspecialchars($row['location_name']) . '</td>
-                    <td>' . htmlspecialchars($row['location_time']) . '</td>
                     <td>' . htmlspecialchars($row['latitude']) . '</td>
                     <td>' . htmlspecialchars($row['longitude']) . '</td>
+                    <td>' . htmlspecialchars($row['type_name']) . '</td>
+                    <td>' . $dayList . '</td>
+                    <td>' . htmlspecialchars($row['location_time']) . '</td>
+                    <td>' . htmlspecialchars($row['usage_count']) . '</td>
                 </tr>';
         $counter++;
     }
@@ -72,41 +99,48 @@ if ($reportType == 'location') {
                 <tr>
                     <th>ลำดับ</th>
                     <th>ชื่อกิจกรรม</th>
-                    <th>วันที่</th>
-                    <th>รายละเอียด</th>
-                    <th>ชื่อสถานที่</th>
-                    <th>ชื่อกีฬา</th>
-                    <th>ชื่อผู้ใช้</th>
-                    <th>สถานะ</th>
+                    <th>ชื่อคนสร้าง</th>
+                    <th>ประเภทกีฬา</th>
+                    <th>วัน/เดือน/ปี</th>
+                    <th>เวลา</th>
+                    <th>จำนวนสมาชิก</th>
+                    <th>รายชื่อสมาชิก</th>
                 </tr>';
 
     $query = "
         SELECT 
             activity.activity_name, 
             activity.activity_date, 
-            activity.activity_details, 
             location.location_name, 
             sport.sport_name, 
-            user_information.user_name, 
-            activity.status 
+            user_information.user_name AS creator_name, 
+            COUNT(member_in_activity.member_id) AS member_count,
+            GROUP_CONCAT(member_info.user_name SEPARATOR ', ') AS member_names
         FROM activity
         JOIN location ON activity.location_id = location.location_id
         JOIN sport ON activity.sport_id = sport.sport_id
-        JOIN user_information ON activity.user_id = user_information.user_id
+        JOIN creator ON activity.activity_id = creator.activity_id
+        JOIN user_information ON creator.user_id = user_information.user_id
+        LEFT JOIN member_in_activity ON activity.activity_id = member_in_activity.activity_id
+        LEFT JOIN user_information AS member_info ON member_in_activity.user_id = member_info.user_id
+        GROUP BY activity.activity_id
     ";
     $result = mysqli_query($conn, $query);
     $counter = 1;
 
     while ($row = mysqli_fetch_assoc($result)) {
+        $activityDate = date('d-m-Y', strtotime($row['activity_date']));
+        $activityTime = date('H:i:s', strtotime($row['activity_date']));
+
         $html .= '<tr>
                     <td>' . $counter . '</td>
                     <td>' . htmlspecialchars($row['activity_name']) . '</td>
-                    <td>' . htmlspecialchars($row['activity_date']) . '</td>
-                    <td>' . htmlspecialchars($row['activity_details']) . '</td>
-                    <td>' . htmlspecialchars($row['location_name']) . '</td>
+                    <td>' . htmlspecialchars($row['creator_name']) . '</td>
                     <td>' . htmlspecialchars($row['sport_name']) . '</td>
-                    <td>' . htmlspecialchars($row['user_name']) . '</td>
-                    <td>' . htmlspecialchars($row['status']) . '</td>
+                    <td>' . htmlspecialchars($activityDate) . '</td>
+                    <td>' . htmlspecialchars($activityTime) . '</td>
+                    <td>' . htmlspecialchars($row['member_count']) . '</td>
+                    <td>' . htmlspecialchars($row['member_names']) . '</td>
                 </tr>';
         $counter++;
     }
@@ -118,32 +152,83 @@ if ($reportType == 'location') {
     $html .= '<table border="1" cellpadding="4">
                 <tr>
                     <th>ลำดับ</th>
-                    <th>รหัสสมาชิก</th>
-                    <th>อีเมล</th>
                     <th>ชื่อสมาชิก</th>
-                    <th>อายุ</th>
-                    <th>สถานะ</th>
+                    <th>วันเกิด</th>
+                    <th>วิธีการสมัคร</th>
+                    <th>สร้างกิจกรรม</th>
+                    <th>เข้าร่วมกิจกรรม</th>
                 </tr>';
 
     $query = "
         SELECT 
-            user_id, 
-            user_email, 
-            user_name, 
-            user_age, 
-            status 
-        FROM user_information";
+            user_information.user_name, 
+            user_information.user_age, 
+            user_information.user_token, 
+            GROUP_CONCAT(DISTINCT created_activity.activity_name SEPARATOR ', ') AS created_activities, 
+            GROUP_CONCAT(DISTINCT joined_activity.activity_name SEPARATOR ', ') AS joined_activities
+        FROM user_information
+        LEFT JOIN creator ON user_information.user_id = creator.user_id
+        LEFT JOIN activity AS created_activity ON creator.activity_id = created_activity.activity_id
+        LEFT JOIN member_in_activity ON user_information.user_id = member_in_activity.user_id
+        LEFT JOIN activity AS joined_activity ON member_in_activity.activity_id = joined_activity.activity_id
+        GROUP BY user_information.user_id
+    ";
+    $result = mysqli_query($conn, $query);
+    $counter = 1;
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $formattedBirthday = date('d-m-Y', strtotime($row['user_age']));
+        $registrationType = $row['user_token'] ? 'สมัครผ่านเฟสบุ๊ค' : 'สมัครธรรมดา';
+
+        $html .= '<tr>
+                    <td>' . $counter . '</td>
+                    <td>' . htmlspecialchars($row['user_name']) . '</td>
+                    <td>' . htmlspecialchars($formattedBirthday) . '</td>
+                    <td>' . htmlspecialchars($registrationType) . '</td>
+                    <td>' . htmlspecialchars($row['created_activities']) . '</td>
+                    <td>' . htmlspecialchars($row['joined_activities']) . '</td>
+                </tr>';
+        $counter++;
+    }
+
+    $html .= '</table>';
+} elseif ($reportType == 'location1') {
+    // ดึงข้อมูลสนามกีฬา
+    $html .= '<h1 style="text-align:center;">รายงานข้อมูลสนามกีฬา</h1>';
+    $html .= '<table border="1" cellpadding="4">
+                <tr>
+                    <th>ลำดับ</th>
+                    <th>ชื่อสนามกีฬา</th>
+                    <th>ละติจูด</th>
+                    <th>ลองจิจูด</th>
+                    <th>ประเภทกีฬา</th>
+                    <th>จำนวนครั้งที่ถูกใช้งาน</th>
+                </tr>';
+
+    $query = "
+        SELECT 
+            location.location_name, 
+            location.latitude, 
+            location.longitude, 
+            sport_type.type_name, 
+            COUNT(activity.activity_id) AS usage_count
+        FROM location
+        LEFT JOIN sport_type_in_location ON location.location_id = sport_type_in_location.location_id
+        LEFT JOIN sport_type ON sport_type_in_location.type_id = sport_type.type_id
+        LEFT JOIN activity ON location.location_id = activity.location_id
+        GROUP BY location.location_id, sport_type.type_id
+    ";
     $result = mysqli_query($conn, $query);
     $counter = 1;
 
     while ($row = mysqli_fetch_assoc($result)) {
         $html .= '<tr>
                     <td>' . $counter . '</td>
-                    <td>' . htmlspecialchars($row['user_id']) . '</td>
-                    <td>' . htmlspecialchars($row['user_email']) . '</td>
-                    <td>' . htmlspecialchars($row['user_name']) . '</td>
-                    <td>' . htmlspecialchars($row['user_age']) . '</td>
-                    <td>' . htmlspecialchars($row['status']) . '</td>
+                    <td>' . htmlspecialchars($row['location_name']) . '</td>
+                    <td>' . htmlspecialchars($row['latitude']) . '</td>
+                    <td>' . htmlspecialchars($row['longitude']) . '</td>
+                    <td>' . htmlspecialchars($row['type_name']) . '</td>
+                    <td>' . htmlspecialchars($row['usage_count']) . '</td>
                 </tr>';
         $counter++;
     }
--- report.php
+++ report.php
@@ -15,32 +15,22 @@ function getLocationData($conn) {
     return $data;
 }
 
-// Function to get detailed location info
+// Function to get detailed location info with usage count
 function getDetailedLocationData($conn) {
-    $query = "SELECT location_name, location_time, location_day, status, latitude, longitude FROM location";
-    $result = mysqli_query($conn, $query);
-    $data = array();
-    while ($row = mysqli_fetch_assoc($result)) {
-        $data[] = $row;
-    }
-    return $data;
-}
-
-// Function to get activity data with names
-function getActivityDataWithNames($conn) {
     $query = "
         SELECT 
-            activity.activity_name, 
-            activity.activity_date, 
-            activity.activity_details, 
             location.location_name, 
-            sport.sport_name, 
-            user_information.user_name, 
-            activity.status 
-        FROM activity
-        JOIN location ON activity.location_id = location.location_id
-        JOIN sport ON activity.sport_id = sport.sport_id
-        JOIN user_information ON activity.user_id = user_information.user_id
+            location.latitude, 
+            location.longitude, 
+            sport_type.type_name,  -- ดึงข้อมูลชื่อประเภทกีฬา
+            location.location_day, 
+            location.location_time, 
+            COUNT(activity.activity_id) AS usage_count
+        FROM location
+        LEFT JOIN sport_type_in_location ON location.location_id = sport_type_in_location.location_id
+        LEFT JOIN sport_type ON sport_type_in_location.type_id = sport_type.type_id  -- ใช้ type_id สำหรับการเชื่อมโยง
+        LEFT JOIN activity ON location.location_id = activity.location_id
+        GROUP BY location.location_id
     ";
     $result = mysqli_query($conn, $query);
     $data = array();
@@ -50,16 +40,31 @@ function getActivityDataWithNames($conn) {
     return $data;
 }
 
-// Function to get user information data
-function getUserInformationData($conn) {
+
+
+// Function to get activity data with names, member count, and member names
+function getActivityDataWithNames($conn) {
     $query = "
         SELECT 
-            user_id, 
-            user_email, 
-            user_name, 
-            user_age, 
-            status 
-        FROM user_information";
+            activity.activity_name, 
+            activity.activity_date, 
+            activity.activity_details, 
+            location.location_name, 
+            sport.sport_name, 
+            user_information.user_name AS creator_name, 
+            activity.status,
+            COUNT(member_in_activity.member_id) AS member_count,
+            GROUP_CONCAT(member_info.user_name SEPARATOR ', ') AS member_names
+        FROM activity
+        JOIN location ON activity.location_id = location.location_id
+        JOIN sport ON activity.sport_id = sport.sport_id
+        JOIN creator ON activity.activity_id = creator.activity_id
+        JOIN user_information ON creator.user_id = user_information.user_id
+        LEFT JOIN member_in_activity ON activity.activity_id = member_in_activity.activity_id
+        LEFT JOIN user_information AS member_info ON member_in_activity.user_id = member_info.user_id
+        GROUP BY activity.activity_id
+        ORDER BY activity.activity_date ASC
+    ";
     $result = mysqli_query($conn, $query);
     $data = array();
     while ($row = mysqli_fetch_assoc($result)) {
@@ -67,6 +72,59 @@ function getUserInformationData($conn) {
     }
     return $data;
 }
+
+
+
+// Function to get user information data with created and joined activities (show activity names)
+function getUserInformationData($conn) {
+    $query = "
+        SELECT 
+            user_information.user_name, 
+            user_information.user_age, 
+            user_information.user_token, 
+            user_information.user_id, 
+            GROUP_CONCAT(DISTINCT created_activity.activity_name SEPARATOR ', ') AS created_activities, 
+            GROUP_CONCAT(DISTINCT joined_activity.activity_name SEPARATOR ', ') AS joined_activities
+        FROM user_information
+        LEFT JOIN creator ON user_information.user_id = creator.user_id
+        LEFT JOIN activity AS created_activity ON creator.activity_id = created_activity.activity_id
+        LEFT JOIN member_in_activity ON user_information.user_id = member_in_activity.user_id
+        LEFT JOIN activity AS joined_activity ON member_in_activity.activity_id = joined_activity.activity_id
+        GROUP BY user_information.user_id
+    ";
+    $result = mysqli_query($conn, $query);
+    $data = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+    return $data;
+}
+// Function to get location report with usage count
+function getLocationReport($conn) {
+    $query = "
+        SELECT 
+            location.location_name, 
+            location.latitude, 
+            location.longitude, 
+            sport_type.type_name, 
+            COUNT(activity.activity_id) AS usage_count
+        FROM location
+        LEFT JOIN sport_type_in_location ON location.location_id = sport_type_in_location.location_id
+        LEFT JOIN sport_type ON sport_type_in_location.type_id = sport_type.type_id
+        LEFT JOIN activity ON location.location_id = activity.location_id
+        GROUP BY location.location_id, sport_type.type_id
+    ";
+    $result = mysqli_query($conn, $query);
+    $data = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+    return $data;
+}
+
+
+
+
 
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -80,6 +138,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $userData = getUserInformationData($conn);
     }
 }
+// สร้างฟังก์ชันสำหรับแปลงหมายเลขวันเป็นชื่อวันภาษาไทย
+function getThaiDayNamesList($days) {
+    $dayNames = array('อาทิตย์', 'จันทร์', 'อังคาร', 'พุธ', 'พฤหัสบดี', 'ศุกร์', 'เสาร์');
+    $dayNumbers = explode(',', $days); // แยกวันทำการที่เก็บเป็นหมายเลขด้วยเครื่องหมายจุลภาค
+    $dayList = '<ul>'; // เริ่มต้นรายการ (list)
+    
+    foreach ($dayNumbers as $dayNumber) {
+        $dayList .= '<li>' . $dayNames[trim($dayNumber)] . '</li>'; // แปลงหมายเลขเป็นชื่อวันและเพิ่มลงในรายการ
+    }
+    
+    $dayList .= '</ul>'; // ปิดรายการ
+    return $dayList; // คืนค่าเป็น HTML ที่เป็นรายการของชื่อวัน
+}
+
 
 ?>
 <!DOCTYPE html>
@@ -309,6 +381,7 @@ button {
             <option value="location">รายงานข้อมูลสถานที่เล่นกีฬา</option>
             <option value="activity">รายงานข้อมูลกิจกรรม</option>
             <option value="user">รายงานข้อมูลสมาชิก</option>
+            <option value="location1">รายงานข้อมูลสนามกีฬา</option>
         </select>
     </div>
 
@@ -322,20 +395,24 @@ button {
 </form>
 
 
-    <div id="reportData">
+<div id="reportData">
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($locationData)) {
         echo "<h2>รายงานข้อมูลสถานที่เล่นกีฬา</h2>";
         echo "<table>";
-        echo "<tr><th>ลำดับ</th><th>ชื่อสถานที่</th><th>เวลาเปิด</th><th>ละติจูด</th><th>ลองจิจูด</th></tr>";
+        echo "<tr><th>ลำดับ</th><th>ชื่อสถานที่</th><th>ละติจูด</th><th>ลองจิจูด</th><th>ประเภทสนามกีฬา</th><th>วันทำการ</th><th>เวลาเปิด-ปิด</th><th>จำนวนครั้งที่ถูกเรียกใช้</th></tr>";
         $counter = 1;
         foreach ($detailedLocationData as $location) {
+            $thaiDayList = getThaiDayNamesList($location['location_day']); // เรียกใช้ฟังก์ชันเพื่อแปลงวันทำการเป็นรายการ
             echo "<tr>";
             echo "<td>" . $counter . "</td>";
             echo "<td>" . htmlspecialchars($location['location_name']) . "</td>";
-            echo "<td>" . htmlspecialchars($location['location_time']) . "</td>";
             echo "<td>" . htmlspecialchars($location['latitude']) . "</td>";
             echo "<td>" . htmlspecialchars($location['longitude']) . "</td>";
+            echo "<td>" . htmlspecialchars($location['type_name']) . "</td>";
+            echo "<td>" . $thaiDayList . "</td>"; // แสดงรายการวันทำการเป็น list
+            echo "<td>" . htmlspecialchars($location['location_time']) . "</td>";
+            echo "<td>" . htmlspecialchars($location['usage_count']) . "</td>";
             echo "</tr>";
             $counter++;
         }
@@ -343,35 +420,60 @@ button {
     } elseif ($_SERVER["REQUEST_METHOD"] == "POST" && isset($activityData)) {
         echo "<h2>รายงานข้อมูลกิจกรรม</h2>";
         echo "<table>";
-        echo "<tr><th>ลำดับ</th><th>ชื่อกิจกรรม</th><th>วันที่</th><th>รายละเอียด</th><th>ชื่อสถานที่</th><th>ชื่อกีฬา</th><th>ชื่อผู้ใช้</th><th>สถานะ</th></tr>";
+        echo "<tr><th>ลำดับ</th><th>ชื่อกิจกรรม</th><th>ชื่อคนสร้าง</th><th>ประเภทกีฬา</th><th>วัน/เดือน/ปี</th><th>เวลา</th><th>จำนวนสมาชิก (คน)</th><th>รายชื่อสมาชิก</th></tr>";
         $counter = 1;
         foreach ($activityData as $activity) {
+            $activityDate = date('d-m-Y', strtotime($activity['activity_date'])); // แยกวันที่
+            $activityTime = date('H:i:s', strtotime($activity['activity_date'])); // แยกเวลา
             echo "<tr>";
             echo "<td>" . $counter . "</td>";
             echo "<td>" . htmlspecialchars($activity['activity_name']) . "</td>";
-            echo "<td>" . htmlspecialchars($activity['activity_date']) . "</td>";
-            echo "<td>" . htmlspecialchars($activity['activity_details']) . "</td>";
-            echo "<td>" . htmlspecialchars($activity['location_name']) . "</td>"; // ชื่อสถานที่
-            echo "<td>" . htmlspecialchars($activity['sport_name']) . "</td>"; // ชื่อกีฬา
-            echo "<td>" . htmlspecialchars($activity['user_name']) . "</td>"; // ชื่อผู้ใช้
-            echo "<td>" . htmlspecialchars($activity['status']) . "</td>";
+            echo "<td>" . htmlspecialchars($activity['creator_name']) . "</td>"; // ชื่อคนสร้าง
+            echo "<td>" . htmlspecialchars($activity['sport_name']) . "</td>"; // ประเภทกีฬา
+            echo "<td>" . htmlspecialchars($activityDate) . "</td>"; // แสดงวันที่
+            echo "<td>" . htmlspecialchars($activityTime) . "</td>"; // แสดงเวลา
+            echo "<td>" . htmlspecialchars($activity['member_count']) . "</td>"; // จำนวนสมาชิก
+            echo "<td>" . htmlspecialchars($activity['member_names']) . "</td>"; // รายชื่อสมาชิก
             echo "</tr>";
             $counter++;
         }
         echo "</table>";
-    } elseif ($_SERVER["REQUEST_METHOD"] == "POST" && isset($userData)) {
+    } elseif($_SERVER["REQUEST_METHOD"] == "POST" && isset($userData)) {
         echo "<h2>รายงานข้อมูลสมาชิก</h2>";
         echo "<table>";
-        echo "<tr><th>ลำดับ</th><th>รหัสสมาชิก</th><th>อีเมล</th><th>ชื่อสมาชิก</th><th>วันเกิด</th><th>สถานะ</th></tr>";
+        echo "<tr><th>ลำดับ</th><th>ชื่อสมาชิก</th><th>วันเกิด</th><th>วิธีการสมัคร</th><th>สร้างกิจกรรม</th><th>เข้าร่วมกิจกรรม</th></tr>";
         $counter = 1;
         foreach ($userData as $user) {
+            $registrationType = $user['user_token'] ? 'สมัครผ่านเฟสบุ๊ค' : 'สมัครธรรมดา';
+            
+            // แปลงวันเกิดเป็นรูปแบบ d-m-Y
+            $formattedBirthday = date('d-m-Y', strtotime($user['user_age']));
+            
             echo "<tr>";
             echo "<td>" . $counter . "</td>";
-            echo "<td>" . htmlspecialchars($user['user_id']) . "</td>";
-            echo "<td>" . htmlspecialchars($user['user_email']) . "</td>";
             echo "<td>" . htmlspecialchars($user['user_name']) . "</td>";
-            echo "<td>" . htmlspecialchars($user['user_age']) . "</td>";
-            echo "<td>" . htmlspecialchars($user['status']) . "</td>";
+            echo "<td>" . htmlspecialchars($formattedBirthday) . "</td>"; // วันเกิดในรูปแบบ d-m-Y
+            echo "<td>" . htmlspecialchars($registrationType) . "</td>"; // วิธีการสมัคร
+            echo "<td>" . htmlspecialchars($user['created_activities']) . "</td>"; // กิจกรรมที่สร้าง
+            echo "<td>" . htmlspecialchars($user['joined_activities']) . "</td>"; // กิจกรรมที่เข้าร่วม
+            echo "</tr>";
+            $counter++;
+        }
+        echo "</table>";
+    } elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $locationReport = getLocationReport($conn);
+        echo "<h2>รายงานข้อมูลสนามกีฬา</h2>";
+        echo "<table>";
+        echo "<tr><th>ลำดับ</th><th>ชื่อสนามกีฬา</th><th>ละติจูด</th><th>ลองจิจูด</th><th>ประเภทกีฬา</th><th>จำนวนครั้งที่ถูกใช้งาน</th></tr>";
+        $counter = 1;
+        foreach ($locationReport as $location1) {
+            echo "<tr>";
+            echo "<td>" . $counter . "</td>";
+            echo "<td>" . htmlspecialchars($location1['location_name']) . "</td>";
+            echo "<td>" . htmlspecialchars($location1['latitude']) . "</td>";
+            echo "<td>" . htmlspecialchars($location1['longitude']) . "</td>";
+            echo "<td>" . htmlspecialchars($location1['type_name']) . "</td>";
+            echo "<td>" . htmlspecialchars($location1['usage_count']) . "</td>";
             echo "</tr>";
             $counter++;
         }
@@ -379,6 +481,8 @@ button {
     }
     ?>
 </div>
+
+
 
 </div>
 
